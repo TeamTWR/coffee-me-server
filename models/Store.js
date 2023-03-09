47700a74844ba8028d4e9148ae3c5121cd7a5ac3
TalOring <tal.oring@eschergroup.com>
2023-03-09 14:41:27 -0500

    Server Release 2

--- models/Store.js
+++ models/Store.js
@@ -1,6 +1,10 @@
 import { Schema, model } from "mongoose";
 
 const StoreSchema = new Schema({
+  createdAt: {
+    type: Date,
+    required: false,
+  },
   storeId: {
     type: String,
     unique: true,
@@ -40,6 +44,10 @@ const StoreSchema = new Schema({
     required: false,
   },
   rating: {
+    type: Number,
+    required: false,
+  },
+  ratingsCount: {
     type: Number,
     required: false,
   },
